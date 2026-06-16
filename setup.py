145a9333c567d41903aecc10e9a05a669d2b1ea0
Prashant Jori <prashantjori745@gmail.com>
2026-06-16 12:47:23 +0530

    requirements

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+setup(
+    name='medical_chatbot',
+    version='0.1.0',
+    author_email="<prashantjori745@gmail.com>",
+    author="Prashant Jori",
+    packages=find_packages(),
+    install_requires=[]
+
+)
