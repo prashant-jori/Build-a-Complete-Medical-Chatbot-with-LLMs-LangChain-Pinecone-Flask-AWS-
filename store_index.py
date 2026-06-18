9bfa5faf1b7d58a353075d7e1e7cfacbaacf8807
Prashant Jori <prashantjori745@gmail.com>
2026-06-18 15:04:29 +0530

    chaneinh import

--- store_index.py
+++ store_index.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv
 import os
-from src.helper import load_pdf_file, filter_to_minimal_docs, text_split, download_hugging_face_embeddings
+from src.helper import load_pdf_files, filter_to_minimal_docs, text_split, download_embeddings
 from pinecone import Pinecone, ServerlessSpec
 from langchain_pinecone import PineconeVectorStore
 
@@ -12,7 +12,8 @@ extracted_data = load_pdf_file(data='data/')
 filter_data = filter_to_minimal_docs(extracted_data)
 text_chunks = text_split(filter_data)
 
-embeddings = download_hugging_face_embeddings()
+
+embeddings = download_embeddings()
 
 pc = Pinecone(api_key=PINECONE_API_KEY)
 
