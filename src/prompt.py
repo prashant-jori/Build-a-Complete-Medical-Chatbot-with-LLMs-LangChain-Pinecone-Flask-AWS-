7f4e1c9c7988c7fa871b4cf0f8d71f5faf0cede7
Prashant Jori <prashantjori745@gmail.com>
2026-06-18 14:55:39 +0530

    changes code

---
+++ src/prompt.py
@@ -0,0 +1,9 @@
+system_prompt = (
+    "You are an Medical assistant for quetion-answering tasks."
+    "Use the following pieces of retrived context to answer"
+    "the quetion. If you don't know the answer, say you don't know." \
+    "dont know.Use three sentences maximum add keep the"
+    "answer concise."
+    "\n\n"
+    "{context}"
+    )
---
+++ store_index.py
@@ -0,0 +1,38 @@
+from dotenv import load_dotenv
+import os
+from src.helper import load_pdf_file, filter_to_minimal_docs, text_split, download_hugging_face_embeddings
+from pinecone import Pinecone, ServerlessSpec
+from langchain_pinecone import PineconeVectorStore
+
+load_dotenv()
+
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+
+extracted_data = load_pdf_file(data='data/')
+filter_data = filter_to_minimal_docs(extracted_data)
+text_chunks = text_split(filter_data)
+
+embeddings = download_hugging_face_embeddings()
+
+pc = Pinecone(api_key=PINECONE_API_KEY)
+
+index_name = "medical-chatbot"
+
+if not pc.has_index(index_name):
+    pc.create_index(
+        name=index_name,
+        dimension=384,
+        metric="cosine",
+        spec=ServerlessSpec(
+            cloud="aws",
+            region="us-east-1"
+        )
+    )
+
+docsearch = PineconeVectorStore.from_documents(
+    documents=text_chunks,
+    embedding=embeddings,
+    index_name=index_name
+)
+
+print("Data uploaded successfully to Pinecone")
